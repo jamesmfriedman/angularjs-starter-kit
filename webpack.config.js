487905fe1dcb2796a33cb613860b7399531fd269
James Friedman <jamesmfriedman@gmail.com>
2016-01-08 09:02:55 -0500

    Html file reloads, config cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,139 @@
 // Some setup
+var fs = require('fs');
+var path = require('path');
 var webpack = require('webpack');
 var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var procName = process.argv[1].split('/').pop();
-var host = 'localhost';
-var port = '8080';
 
-if (process.argv.indexOf('--host') != -1) {
-	host = process.argv[process.argv.indexOf('--host') + 1];
+function HelloWorldPlugin(options) {
+  // Setup the plugin instance with options...
 }
 
-if (process.argv.indexOf('--port') != -1) {
-	port = process.argv[process.argv.indexOf('--port') + 1];
-}
-
-var hostPort = host + ':' + port;
-
-// Our main config
-var config = {
-	context: __dirname,
-	entry: {
-		app: ['./src/main', './src/main.scss']
-	},
-	output: {
-		path: './public/dist',
-		filename: '[name].js', // no hash in main.js because index.html is a static page
-		publicPath: '/dist'
-    },
-    resolve: {
-		modulesDirectories: ['src', 'web_modules', 'node_modules']
-	},
-	module: {
-		loaders: [
-			{
-		        test: /\.scss$/,
-		        loaders: ['style/url','file?name=/app.css?[hash]', 'autoprefixer', 'sass', 'import-glob']
-		    }
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('[name].css')
-	]
+HelloWorldPlugin.prototype.apply = function(compiler) {
+	
+	compiler.plugin('compilation', function() {
+		console.log('Compiled', new Date().toLocaleTimeString());
+	});
 };
 
-// Dev Config Aadditions
-if (procName == 'webpack-dev-server') {
-	config.devtool = '#source-map';
-	config.output.sourceMapFilename = '[name].map.js';
+function WebpackConfig() {
+	this.config = {};
+
+	this.host = 'localhost';
+	this.port = '8080';
+	this.hostPort = undefined;
+	this.procName = process.argv[1].split('/').pop();
 	
-	config.entry.app.unshift('webpack-dev-server/client?http://' + hostPort);
-	config.entry.app.unshift('webpack/hot/dev-server');
-	
-	//conf
-	config.devServer = {
-		contentBase: __dirname + '/public',
-		headers: { 
-			//To add headers to the dev server "Access-Control-Allow-Origin": '*', 
-		},
-		hot: true,
-		noInfo: true,
-		stats: {
-			colors: true
+	this.constructor = function() {
+		if (process.argv.indexOf('--host') != -1) {
+			this.host = process.argv[process.argv.indexOf('--host') + 1];
 		}
 
-		//OPTIONAL PROXY SERVER https://webpack.github.io/docs/webpack-dev-server.html#proxy
-	   	// proxy: {
-     //        '*': {
-     //            target: 'http://example.com',
-     //            secure: false,
-     //        },
-     //    },
-	};
+		if (process.argv.indexOf('--port') != -1) {
+			this.port = process.argv[process.argv.indexOf('--port') + 1];
+		}
 
-	config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+		this.hostPort = this.host + ':' + this.port;
 
+		this.generalConfig();
+		if (this.procName == 'webpack-dev-server') {
+			this.devConfig();
+		}
+
+		if (this.procName == 'webpack') {
+			this.buildConfig();
+		}
+	}
 	
+	/**
+	 * Generic config object, gets applied to everything
+	 */
+	this.generalConfig = function() {
+		this.config = {
+			context: __dirname,
+			entry: {
+				app: ['./src/main', './src/main.scss']
+			},
+			output: {
+				path: './public/dist',
+				filename: '[name].js', // no hash in main.js because index.html is a static page
+				publicPath: '/dist'
+		    },
+		    resolve: {
+				modulesDirectories: ['src', 'web_modules', 'node_modules']
+			},
+			module: {
+				loaders: [
+					{
+				        test: /\.scss$/,
+				        loaders: ['style/url','file?name=/app.css?[hash]', 'autoprefixer', 'sass', 'import-glob']
+				    }
+				]
+			},
+			plugins: [
+				new ExtractTextPlugin('[name].css'),
+				new HelloWorldPlugin()
+			]
+		}
+	}
+
+	/**
+	 * Additions for webpack-dev-server only
+	 */
+	this.devConfig = function() {
+		// watch html files
+		this.config.entry.app.unshift('./src/templates');
+		this.config.module.loaders.push({
+		   test: /\.html$/,
+		   loader: 'raw-loader'
+		});
+
+		// source maps
+		this.config.devtool = '#source-map';
+		this.config.output.sourceMapFilename = '[name].map.js';
+		
+		// hot mode
+		this.config.entry.app.unshift('webpack-dev-server/client?http://' + this.hostPort);
+		this.config.entry.app.unshift('webpack/hot/dev-server');
+		this.config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+		
+		// dev server conf
+		this.config.devServer = {
+			contentBase: __dirname + '/public',
+			headers: { 
+				//To add headers to the dev server "Access-Control-Allow-Origin": '*', 
+			},
+			hot: true,
+			noInfo: true,
+			stats: {
+				colors: true
+			}
+
+			// OPTIONAL PROXY SERVER https://webpack.github.io/docs/webpack-dev-server.html#proxy
+			// proxy: {
+			//     '*': {
+			//         target: 'http://example.com',
+			//         secure: false,
+			//     },
+			// },
+		};
+	}
+
+	/**
+	 * Additions for webpack only
+	 */
+	this.buildConfig = function() {
+		this.config.plugins = this.config.plugins.concat([
+			new ngAnnotatePlugin({
+	            add: true
+	        }),
+			new webpack.optimize.UglifyJsPlugin()
+		]);
+	}
+
+	this.constructor();
 }
 
-if (procName == 'webpack') {
-	config.plugins = config.plugins.concat([
-		new ngAnnotatePlugin({
-            add: true
-        }),
-		new webpack.optimize.UglifyJsPlugin()
-	]);
-}
 
-
-module.exports = config;
+module.exports = new WebpackConfig().config;
